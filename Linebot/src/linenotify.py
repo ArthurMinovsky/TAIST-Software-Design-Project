6d8f6120d8a1acc9c05b97aa31ba65c4a3d56ca8
Panisara Kanjanarut <114897171+havesometomato@users.noreply.github.com>
2023-04-06 01:58:22 +0700

    Switch language

--- Linebot/src/linenotify.py
+++ Linebot/src/linenotify.py
@@ -10,13 +10,13 @@ app = FastAPI()
 @app.get("/api/route")
 def route(num_car):
     if num_car == "0":
-        msg = 'กรุณาเข้าจอดในช่อง A'
+        msg = 'Your Stall ID is A'
     if num_car == "1":
-        msg = 'กรุณาเข้าจอดในช่อง B'
+        msg = 'Your sStall ID is B'
     if num_car == "2":
-        msg = 'กรุณาเข้าจอดในช่อง C'
+        msg = 'Your Stall ID is C'
     if num_car == "3":
-        msg = 'กรุณาเข้าจอดในช่อง D'
+        msg = 'Your Stall ID is D'
     r = requests.post(url, headers=headers, data = {'message':msg})
     return "sloting OK"     #200 is OK
 
@@ -25,11 +25,11 @@ db_num = 0
 @app.get("/api/displayid")
 def displayid(id ,bool(car_status)):
     if car_status == True:
-        msg = 'ยินดีต้อนรับคุณเข้าสู่ระบบจอดรถ'
+        msg = 'Welcome to our parking lot system' 
         r = requests.post(url, headers=headers, data = {'RFID':id})
         route(db_num.count())
     if car_status == False:
-        msg = 'ขออภัยที่จอดรถเต็มแล้ว'
+        msg = 'Sorry, we have run out of parking stalls'
         r = requests.post(url, headers=headers, data = {'massage':msg})
     return "display OK"     #200 is OK
 
