e592a299ad1bcadc550d883e2117bfcba4169bda
Kornchanok Krajangyao <126540644+KornnoK@users.noreply.github.com>
2023-04-06 13:36:49 +0700

    Update and rename VER2_buttonAddRYG.ino to LED_state.ino

---
+++ mqtt/LED_state.ino
@@ -0,0 +1,94 @@
+#define BUTTON_PIN_1 26
+#define BUTTON_PIN_2 27
+#define LED_PIN 33
+#define LED_R_1 18
+#define LED_Y_1 19
+#define LED_G_1 21
+
+int button_state1;
+int button_state2;
+int led_state = LOW; 
+int last_button_state;
+int last_button_state2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN_1, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_2, INPUT_PULLUP);  
+
+  pinMode(LED_R_1, OUTPUT); 
+  pinMode(LED_Y_1, OUTPUT);
+  pinMode(LED_G_1, OUTPUT);
+  
+  digitalWrite(LED_R_1, HIGH);
+  digitalWrite(LED_Y_1, LOW);
+  digitalWrite(LED_G_1, LOW);
+
+  button_state1 = digitalRead(BUTTON_PIN_1);
+  button_state2 = digitalRead(BUTTON_PIN_2);
+}
+
+void buttonWait(int buttonPin){
+  int buttonState = 0;
+  while(1){
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == LOW) {
+      return;
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  last_button_state = button_state1;
+  button_state1 = digitalRead(BUTTON_PIN_1);
+
+  if (last_button_state == HIGH && button_state1 == LOW) {
+    Serial.println("The button is pressed");
+    // toggle state of LED
+    led_state = !led_state;
+    Serial.println(led_state);
+    // control LED arccoding to the toggled state
+    digitalWrite(LED_PIN, led_state);
+    
+    if (led_state == 1){
+
+      //CODE RFID for carOUT//
+      
+      digitalWrite(LED_R_1, LOW);
+      digitalWrite(LED_Y_1, HIGH);
+      digitalWrite(LED_G_1, LOW);
+      delay(1000);
+      last_button_state2 = button_state2;
+      button_state2 = digitalRead(BUTTON_PIN_2);
+      Serial.println(button_state2);
+      buttonWait(27);
+      if (button_state2 == HIGH){
+        digitalWrite(LED_R_1, LOW);
+  	digitalWrite(LED_Y_1, LOW);
+	digitalWrite(LED_G_1, HIGH);
+        delay(2000);
+      }       
+    }
+    else{
+      buttonWait(27); 
+      digitalWrite(LED_R_1, LOW);
+      digitalWrite(LED_Y_1, HIGH);
+      digitalWrite(LED_G_1, LOW);     
+
+      //RFID For car in//
+
+      buttonWait(27);      
+      digitalWrite(LED_R_1, LOW);
+      digitalWrite(LED_Y_1, LOW);
+      digitalWrite(LED_G_1, HIGH);  
+      delay(5000);
+    }
+  }
+  digitalWrite(LED_R_1, HIGH);
+  digitalWrite(LED_Y_1, LOW);
+  digitalWrite(LED_G_1, LOW);  
+}
